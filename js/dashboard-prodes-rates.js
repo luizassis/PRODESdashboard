88b04344a242fc5db2614781211b124965fe102f
Luiz Fernando Ferreira Gomes de Assis <luizffga@dpi.inpe.br>
2017-10-31 14:25:06 -0200

    update collapsePanel call

--- js/dashboard-prodes-rates.js
+++ js/dashboard-prodes-rates.js
@@ -67,7 +67,7 @@ var utils = {
 		// occurs when the collapsible element is about to be shown or to be hidden
 		selectIds.on('show.bs.collapse hidden.bs.collapse', function () {
 			// alternate between plus and minus
-			$(this).prev().find('.glyphicon').toggleClass('glyphicon-plus glyphicon-minus'); 
+			$(this).prev('.panel-heading').find('.glyphicon').toggleClass('glyphicon-plus glyphicon-minus'); 
 		});
 	}
 };
@@ -418,6 +418,8 @@ var graph={
 			// TODO: handle exception
 			
 		}
+		utils.collapsePanel();
+
 	},
 	/*
 	 * Called from the UI controls to clear one specific filter.
@@ -484,8 +486,6 @@ var graph={
 	    	window.location='?type=default';
 		});
 
-		utils.collapsePanel();		
-		
 		this.jsLanguageChange();
 	},
 	preparePrint: function() {
